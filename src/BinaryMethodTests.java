8c56cad69c800e661cce993cdb85e746fae2dd9d
angieArt23 <angart1223@gmail.com>
2022-04-05 16:50:18 -0700

    changes to Tests

--- src/BinaryMethodTests.java
+++ src/BinaryMethodTests.java
@@ -1,5 +1,14 @@
 package src;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+
 /**
  * This is the testing class we use to test the methods we implemented for this project.
  *
@@ -15,21 +24,85 @@ package src;
  */
 public class BinaryMethodTests {
 
+    BinaryTree<String> tree = new BinaryTree<String>();
+
+    // Leaves:
+	BinaryTree<String> bTree = new BinaryTree<>("B");
+	BinaryTree<String> fTree = new BinaryTree<>("F");
+	BinaryTree<String> gTree = new BinaryTree<>("G");
+	BinaryTree<String> hTree = new BinaryTree<>("H");
+
+	// Subtrees:
+	BinaryTree<String> dTree = new BinaryTree<>("D", fTree, null);
+	BinaryTree<String> eTree = new BinaryTree<>("E", gTree, hTree);
+	BinaryTree<String> cTree = new BinaryTree<>("C", dTree, eTree);
+
     // Test Postorder traversal in BinaryTree
+    @Test
+    public void testPostorderTraverse() throws Exception {
+
+        tree.setTree("A", bTree, cTree);
+
+
+    }
+
     // Test Postorder traversal in BinaryNode
+    @Test
+    public void testPostorderTraverse_callBinaryNodeMethod(){
+
+    }
 
     // Test Preorder traversal in BinaryTree
+    @Test
+    public void testPreorderTraverse(){
+
+    }
+
     // Test Preorder traversal in BinaryNode
+    @Test    
+    public void testPreorderTraverse_callBinaryNodeMethod(){
+    
+    }
 
     // Test Inorder traversal in BinaryTree
+    @Test
+    public void testInorderTraverse(){
+
+    }
+
     // Test Inorder traversal in BinaryNode
+    @Test   
+    public void testInorderTraverse_callBinaryNodeMethod(){
+
+    }
 
     // Test height calculation in BinaryTree
+    @Test
+    public void testHeight(){
+
+    }
+
     // Test height calculation in BinaryNode
+    @Test
+    public void testHeight_callBinaryNodeMethod(){
+
+    }
 
     // Test node calculation in BinaryTree
+    @Test
+    public void testNode(){
+
+    }
     // Test node calculation in BinaryNode
+    @Test
+    public void testNode_callBinaryNodeMethod(){
+
+    }
 
     // Test BinaryTree(preorder, inorder) constructor
+    @Test
+    public void testBinaryTree(){
+
+    }
 
 }
