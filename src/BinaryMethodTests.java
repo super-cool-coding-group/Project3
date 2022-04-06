bdfdfb0bac8bd0bcd2f5956c5f1b2874ac57eef7
angieArt23 <angart1223@gmail.com>
2022-04-06 01:03:42 -0700

    changes to tests

--- src/BinaryMethodTests.java
+++ src/BinaryMethodTests.java
@@ -1,12 +1,6 @@
 package src;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
-
 import org.junit.Test;
 
 /**
@@ -39,22 +33,29 @@ public class BinaryMethodTests {
 
     // Test Postorder traversal in BinaryTree
     @Test
-    public void testPostorderTraverse() throws Exception {
-
+    public void testPostorderTraverse(){
         tree.setTree("A", bTree, cTree);
-
-
+        String expected = "BFDGHECA";
+        String test = tree.postorderTraverse_test();
+        assertEquals(expected, test);
     }
 
     // Test Postorder traversal in BinaryNode
     @Test
     public void testPostorderTraverse_callBinaryNodeMethod(){
-
+        tree.setTree("A", bTree, cTree);
+        String expected = "BFDGHECA";
+        String test = tree.postorderTraverse_callBinaryNodeMethod();
+        assertEquals(expected, test);
     }
 
     // Test Preorder traversal in BinaryTree
     @Test
     public void testPreorderTraverse(){
+        tree.setTree("A", bTree, cTree);
+        String expected = "ABCDFEGH";
+        String test = tree.preorderTraverse_test();
+        assertEquals(expected, test);
 
     }
 
@@ -67,6 +68,10 @@ public class BinaryMethodTests {
     // Test Inorder traversal in BinaryTree
     @Test
     public void testInorderTraverse(){
+        tree.setTree("A", bTree, cTree);
+        String expected = "BAFDCGEH";
+        String test = tree.inorderTraverse_test();
+        assertEquals(expected, test);
 
     }
 
@@ -78,17 +83,57 @@ public class BinaryMethodTests {
 
     // Test height calculation in BinaryTree
         // test when height should be 0, 1, and any other number
-
     @Test
     public void testHeight(){
+        // test height = 0
+		BinaryTree<String> tree = new BinaryTree<>();
+        int expected = 0;
+        int test = tree.getHeight();
+        assertEquals(expected, test);
 
+        // test height = 1
+        BinaryTree<String> tree2 = new BinaryTree<>("A");
+        int expected2 = 1;
+        int test2 = tree2.getHeight();
+        assertEquals(expected2, test2);
+
+        // test height = 4
+        BinaryTree<String> tree3 = new BinaryTree<>();
+        tree3.setTree("A", bTree, cTree);
+        int expected3 = 4;
+        int test3 = tree3.getHeight();
+        assertEquals(expected3, test3);
     }
 
     // Test height calculation in BinaryNode
         // test when height should be 0, 1, and any other number.
-        // also check a node that is within a tree but is not necessarily
+        // also check a node that is within a tree but is not necessarily the root of the tree
     @Test
     public void testHeight_callBinaryNodeMethod(){
+        BinaryTree<String> tree = new BinaryTree<>();
+        int expected = 0;
+        int test = tree.getHeight_callBinaryNodeMethod();
+        assertEquals(expected, test);
+
+        // test height = 1
+        BinaryTree<String> tree2 = new BinaryTree<>("A");
+        int expected2 = 1;
+        int test2 = tree2.getHeight_callBinaryNodeMethod();
+        assertEquals(expected2, test2);
+
+        // test height = 4
+        BinaryTree<String> tree3 = new BinaryTree<>();
+        tree3.setTree("A", bTree, cTree);
+        int expected3 = 4;
+        int test3 = tree3.getHeight_callBinaryNodeMethod();
+        assertEquals(expected3, test3);
+
+        // test node
+        BinaryTree<String> tree4 = new BinaryTree<>();
+        tree4.setTree("A", bTree, cTree);
+        int expected4 = 4;
+        int test4 = tree4.getHeight_callBinaryNodeMethod();
+        assertEquals(expected4, test4);
 
     }
 
